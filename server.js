7f21d04fa5a0f52e27be6c93461997f4cb82db08
vikasazureit <vikas.azureit@gmail.com>
2026-08-24 15:10:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const today = new Date();
+
+    const day = today.toLocaleDateString("en-IN", {
+        weekday: "long"
+    });
+
+    const date = today.toLocaleDateString("en-IN", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(`
+        <html>
+            <head>
+                <title>ADO Demo</title>
+            </head>
+            <body>
+                <h1>Hello from Azure DevOps!</h1>
+                <h2>Today is ${day}</h2>
+                <h2>Date is ${date}</h2>
+            </body>
+        </html>
+    `);
+});
+
+server.listen(3000, () => {
+    console.log("Application running on http://localhost:3000");
+});
